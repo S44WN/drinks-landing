ace435cbd7a7d736fa945308c03eace750553599
Saawn <itissaavn@gmail.com>
2024-12-25 01:00:17 +0530

    Enhance AlternatingTextScene with responsive animations and updated background colors

--- src/components/AlternatingText/AlternatinTextScene.tsx
+++ src/components/AlternatingText/AlternatinTextScene.tsx
@@ -8,6 +8,7 @@ import { Group } from "three";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
+import { useMediaQuery } from "@/hooks/useMediaQuery";
 
 gsap.registerPlugin(ScrollTrigger, useGSAP);
 
@@ -15,15 +16,63 @@ type Props = object;
 
 function AlternatinTextScene({}: Props) {
   const canRef = useRef<Group>(null);
+  const isDesktop = useMediaQuery("(min-width: 768px)", true);
 
-  const bgColors = ["#710523", "#572981", "#164405", "#690B3D", "#4B7002"];
+  const bgColors = ["#710523", "#FFCFCF", "#86A788"];
 
-  useGSAP(() => {
-    if (!canRef.current) return;
-  });
+  useGSAP(
+    () => {
+      if (!canRef.current) return;
+
+      const sections = gsap.utils.toArray(".alternating-section");
+
+      const scrollTl = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".alternating-text-view",
+          endTrigger: ".alternating-text-container",
+          pin: true,
+          start: "top top",
+          end: "bottom bottom",
+          scrub: true,
+        },
+      });
+
+      sections.forEach((_, index) => {
+        if (!canRef.current) return;
+        if (index === 0) return;
+
+        const isOdd = index % 2 !== 0;
+
+        const xPosition = isDesktop ? (isOdd ? "-1" : "1") : 0;
+        const yRotation = isDesktop ? (isOdd ? ".4" : "-.4") : 0;
+        scrollTl
+          .to(canRef.current.position, {
+            x: xPosition,
+            ease: "circ.inOut",
+            delay: 0.5,
+          })
+          .to(
+            canRef.current.rotation,
+            {
+              y: yRotation,
+              ease: "back.inOut",
+            },
+            "<"
+          )
+          .to(".alternating-text-container", {
+            backgroundColor: gsap.utils.wrap(bgColors, index),
+          });
+      });
+    },
+    { dependencies: [isDesktop] }
+  );
 
   return (
-    <group ref={canRef}>
+    <group
+      ref={canRef}
+      position-x={isDesktop ? 1 : 0}
+      rotation-y={isDesktop ? -0.3 : 0}
+    >
       <FloatingCan flavor="strawberryLemonade" />
       <Environment files={"/hdrs/lobby.hdr"} environmentIntensity={1.5} />
     </group>
