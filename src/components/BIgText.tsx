708447c49e84991809fbe9ca2041416a9642528e
Saawn <itissaavn@gmail.com>
2024-12-31 18:57:49 +0530

    Add spin animation and update background colors in BigText and Footer components

--- src/components/BIgText.tsx
+++ src/components/BIgText.tsx
@@ -8,7 +8,7 @@ export type BigTextProps = object;
  */
 const BigText = ({}: BigTextProps): JSX.Element => {
   return (
-    <section className="min-h-screen w-screen overflow-hidden bg-[#FE6334] text-[#FEE832]">
+    <section className="min-h-screen w-screen overflow-hidden bg-[#fff2ee] text-[#ff9777]">
       <h2 className="grid w-full gap-[3vw] py-10 text-center font-black uppercase leading-[.7]">
         <div className="text-[34vw]">Soda</div>
         <div className="grid gap-[3vw] text-[34vw] md:flex md:text-[11vw]">
